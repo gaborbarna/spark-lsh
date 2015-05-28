afe6e91c1f4b99562224fb387b7832bda75340c5
Barna Gabor <mr.barna.gabor@gmail.com>
2015-05-28 18:18:20 +0200

    lsh bands

--- src/main/scala/LSH.scala
+++ src/main/scala/LSH.scala
@@ -7,12 +7,18 @@ import org.apache.spark.rdd.RDD
 class LSH(n: Int, p: Int, m: Int, bands: Int) extends Serializable {
   val hashers = (0 until n).map(i => (Hash.getHasher(p, m), i))
 
-  def fit(data: RDD[SparseVector]) = {
+  def fit(data: RDD[SparseVector], minClusterSize: Int = 2) = {
+    val bands = getBands(data)
+    bands.filter({case (_, v) => v.size >= minClusterSize}).map({
+      case (_, v) => v.sorted
+    }).distinct()
+  }
+
+  def getBands(data: RDD[SparseVector]) = {
     val dataWithIndex = data.zipWithIndex().cache()
-    val minHashes = dataWithIndex.flatMap({case (v, v_i) => {
+    dataWithIndex.flatMap({case (v, v_i) => {
       hashers.map({case (h, h_i) => ((v_i, h_i % bands), (Vector(Hash.minHash(v, h)), v))})
-    }})
-    minHashes.reduceByKey({case ((acc, _), (h, v)) => (acc ++ h, v)}).map({case ((v_i, band), (h, v)) => {
+    }}).reduceByKey({case ((acc, _), (h, v)) => (acc ++ h, v)}).map({case ((v_i, band), (h, v)) => {
       ((band, h), v)
     }}).groupByKey().cache()
   }
